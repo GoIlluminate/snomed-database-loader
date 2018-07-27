48a06a6e43654d4e5a4c88ee8fcb4a0111569a59
Chloe-H <chloe.henderson@goilluminate.com>
2018-07-27 14:12:12 -0500

    Added options with arguments for specifying the SNOMED release and database configurations, as well as showing a help menu.

--- PostgreSQL/load_release-postgresql.sh
+++ PostgreSQL/load_release-postgresql.sh
@@ -3,18 +3,111 @@ set -e;
 
 cd "`dirname "$0"`"
 baseDir="$(pwd)"
-releasePath=$1
-dbName=$2
-releaseType=$3
-moduleStr=$4
-dbUsername=$5
-dbPort=$6
 
-if [ -z ${moduleStr} ]
-then
-	echo "Usage <release location> <db schema name> <DELTA|SNAP|FULL|ALL> <module string> [<postgres username> <postgres port>]"
-	exit -1
-fi
+# lookit all these variables for configurations
+declare releasePath
+declare releaseType
+declare moduleName
+declare dbName
+declare dbHost
+declare dbPort
+declare dbUsername
+
+function showErrorMessage() {
+    echo "$1"
+    echo ""
+    echo "Try '$0 -H' for more information."
+}
+
+while getopts ":d:h:Hl:m:p:t:u:" opt; do
+    case ${opt} in
+        d )
+            dbName=$OPTARG
+            ;;
+        h )
+            dbHost=$OPTARG
+            ;;
+        H )
+            echo "Usage: $0 CONFIGURATIONS"
+            echo "  or:  $0 -H"
+            echo ""
+            echo "Create and populate a PostgreSQL database with a SNOMED CT terminology release."
+            echo ""
+            echo "== Configurations (required) =="
+            echo ""
+            echo "SNOMED CT release configurations:"
+            echo "  -l        release archive location"
+            echo "  -m        module name (e.g. INT, US1000124)"
+            echo "  -t        release type"
+            echo "               options are DELTA, SNAP, FULL, or ALL"
+            echo "               SNAP is for snapshot, and ALL is for delta, snapshot, and full"
+            echo ""
+            echo "Database configurations:"
+            echo "  -d        database name to connect to"
+            echo "  -h        database server host or socket directory"
+            echo "  -p        database server port"
+            echo "  -u        database user name"
+            echo ""
+            echo "== Other (optional)"
+            echo ""
+            echo "  -H        display this help and exit"
+
+            exit 1
+            ;;
+        l )
+            releasePath="`dirname "$OPTARG"`"
+            ;;
+        m )
+            moduleName=$OPTARG
+            ;;
+        p )
+            dbPort=$OPTARG
+            ;;
+        t )
+            releaseType=$OPTARG
+            ;;
+        u )
+            dbUsername=$OPTARG
+            ;;
+        : ) # missing option argument
+            declare missingArgDesc
+
+            case ${OPTARG} in
+                d )
+                    missingArgDesc="the database name"
+                    ;;
+                h )
+                    missingArgDesc="the database server host or socket directory"
+                    ;;
+                l )
+                    missingArgDesc="the path to the SNOMED archive"
+                    ;;
+                m )
+                    missingArgDesc="the name of the SNOMED module"
+                    ;;
+                p )
+                    missingArgDesc="the database server port"
+                    ;;
+                t )
+                    missingArgDesc="the type of the SNOMED release"
+                    ;;
+                u )
+                    missingArgDesc="the database user name"
+                    ;;
+            esac
+
+            showErrorMessage "Option -$OPTARG requires $missingArgDesc as an argument."
+
+            exit -1
+            ;;
+
+        \? ) # invalid option
+            showErrorMessage "$0: invalid option -- '$OPTARG'"
+            ;;
+    esac
+done
+
+
 
 # Unzip the files here, junking the structure
 localExtract="tmp_extracted"
@@ -35,7 +128,7 @@ case "${releaseType}" in
 	'ALL') fileTypes=(Delta Snapshot Full)	
 		unzip -j ${releasePath} -d ${localExtract}
 	;;
-	*) echo "Release type ${releaseType} not recognised"
+	*) echo "Release type '${releaseType}' not recognised"
 	exit -1;
 	;;
 esac
@@ -48,7 +141,7 @@ function addLoadScript() {
 	for fileType in ${fileTypes[@]}; do
 		fileName=${1/TYPE/${fileType}}
 		fileName=${fileName/DATE/${releaseDate}}
-        fileName=${fileName/INT/${moduleStr}}
+        fileName=${fileName/INT/${moduleName}}
 
 		# Check file exists - try beta version if not
 		if [ ! -f ${localExtract}/${fileName} ]; then
@@ -82,7 +175,7 @@ addLoadScript der2_cRefset_AttributeValueTYPE_INT_DATE.txt attributevaluerefset
 addLoadScript der2_cRefset_LanguageTYPE-en_INT_DATE.txt langrefset
 addLoadScript der2_cRefset_AssociationTYPE_INT_DATE.txt associationrefset
 
-psql -h localhost -U ${dbUsername} -p ${dbPort} -d ${dbName} << EOF
+psql -h ${dbHost} -U ${dbUsername} -p ${dbPort} -d ${dbName} << EOF
 	\ir create-database-postgres.sql;
 	\ir environment-postgresql.sql;
 	\ir ${generatedLoadScript};
